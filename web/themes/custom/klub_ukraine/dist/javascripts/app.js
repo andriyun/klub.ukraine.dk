40261f2d39e9392db4e08667d281066d0c1ccd19
andriyun <andriy.yun@gmail.com>
2022-03-30 22:22:35 +0200

    Added theme

---
+++ web/themes/custom/klub_ukraine/dist/javascripts/app.js
@@ -0,0 +1,5 @@
+jQuery(function ($) {
+  'use strict';
+});
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyJqUXVlcnkoZnVuY3Rpb24gKCQpIHtcbiAgJ3VzZSBzdHJpY3QnO1xufSk7XG4iXX0=
